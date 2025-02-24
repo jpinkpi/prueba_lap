17418b092b130bc79920657c0ef711fb2b34f520
jpinkpi <josepablorivera002@gmail.com>
2025-02-24 15:03:19 -0700

    Fix: Set Render port

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import os
 import streamlit as st
+
+# Obtener el puerto que Render asigna
+port = int(os.environ.get("PORT", 8501))
 
 st.header('Lanzar una moneda')
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
@@ -6,5 +10,9 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-    
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
+
+# Lanzar Streamlit en el puerto correcto
+if __name__ == "__main__":
+    st.run(port=port)
